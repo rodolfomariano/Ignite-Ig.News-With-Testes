9b4ac80e3cc33595618e5618c394dfa19b8580a9
Rodolfo Mariano de Souza <rodolfo.mariano@outlook.com>
2021-06-03 13:19:05 -0300

    Vercel test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "da7a8a5bb768346a8f4e";
+/******/ 	var hotCurrentHash = "9ec54a59549f67654bc6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
